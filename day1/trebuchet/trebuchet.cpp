02aa4663259da6b060bf9c4508af101f3cd861cc
mgiles717 <mgiles717@gmail.com>
2023-12-04 18:16:44 +0000

    Trebuchet complete

--- day1/trebuchet/trebuchet.cpp
+++ day1/trebuchet/trebuchet.cpp
@@ -3,30 +3,67 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <numeric>
 
 using namespace std;
+
+const int NOT_FOUND = -1;
 
 // Pass logic whilst loading to avoid having to deal with parsing 
 // lines after alreaady using getline
 // 
 // Make vector to dynamically store the number corresponding to 
 // the first and last digit (too lazy to allocate size for static array)
-string loadFile(std::string file_path){
-    string fileContent;
+vector<int> loadFile(std::string file_path){
     ifstream myFile(file_path);
-    if (myFile.is_open()){
-        while (getline(myFile, fileContent)){
-            cout << fileContent << '\n';
-        }
-    myFile.close();
-    }
-    else cout << "Unable to open the file!";
 
-    return fileContent;
+    if (!myFile.is_open()){
+        cout << "Unable to open the file!" << endl;
+        return {};
+    }
+
+    vector<int> line_digits {};
+
+    // Iterate through each line of the file
+    string line;
+    while (getline(myFile, line)){
+        int first = NOT_FOUND;
+        int last = NOT_FOUND;
+        int combined = 0;
+        for(char c : line){
+            if(isdigit(c) == true){
+                // Convert to numeric
+                if(first == NOT_FOUND){
+                    first = c - '0';
+                }
+                else{
+                    last = c - '0';
+                }
+            }   
+        }
+        
+        // Check if not combining
+        // if not, increase to 2nd order base10 and add first
+        // first = 7, last = , combined = 77
+        if (last == -1){
+            combined = (first * 10) + first;
+        }
+        // Shift first to 2nd order base10 to combined as 2 digit number
+        else{
+            combined = (first * 10) + last;
+        }
+
+        line_digits.push_back(combined); 
+    }
+    
+    myFile.close();
+    return line_digits;
 }
 
 int main(){
-    string fileContent = loadFile("inputs.txt");
-    cout << fileContent;
+    vector<int> digits = loadFile("inputs.txt");
+    int sum = accumulate(digits.begin(), digits.end(), 0);
+    
+    cout << sum << endl;
     return 0;
 }
